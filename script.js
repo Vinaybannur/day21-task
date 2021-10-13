f8086213d6e6e7b264aea7c33d150ea1cd7e609b
Vinaybannur <91072399+Vinaybannur@users.noreply.github.com>
2021-10-13 09:35:51 +0530

    callback hell

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(function cb1(){
+    document.querySelector(".count").innerText="10";
+    setTimeout(function cb2(){
+      document.querySelector(".count").innerText="9";
+      setTimeout(function cb3(){
+       document.querySelector(".count").innerText="8";
+        setTimeout(function cb4(){
+          document.querySelector(".count").innerText="7";
+          setTimeout(function cb5(){
+            document.querySelector(".count").innerText="6";
+            setTimeout(function cb6(){
+              document.querySelector(".count").innerText="5";
+              setTimeout(function cb7(){
+                document.querySelector(".count").innerText="4";
+                setTimeout(function cb8(){
+                 document.querySelector(".count").innerText="3";
+                  setTimeout(function cb9(){
+                     document.querySelector(".count").innerText="2";
+                    setTimeout(function cb10(){
+                      document.querySelector(".count").innerText="1";
+                      setTimeout(function cb11(){
+                        document.querySelector(".count").innerText="Happy Independence Day!!!";
+                         },1000*1);
+                       },1000*1);
+                     },1000*1);
+                    },1000*1);
+                  },1000*1);
+                },1000*1);
+              },1000*1);
+            },1000*1);
+          },1000*1);
+        },1000*1);
+     },1000*1);
